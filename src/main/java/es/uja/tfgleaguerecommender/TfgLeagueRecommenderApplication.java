4bcf0eb7e3c3bbf21333f2daf5c8d298b73be8ca
Rafael Reinoso Díaz <rrd00018@red.ujaen.es>
2025-11-15 15:33:12 +0100

    First Commit

---
+++ src/main/java/es/uja/tfgleaguerecommender/TfgLeagueRecommenderApplication.java
@@ -0,0 +1,12 @@
+package es.uja.tfgleaguerecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgLeagueRecommenderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TfgLeagueRecommenderApplication.class, args);
+  }
+}
---
+++ src/test/java/es/uja/tfgleaguerecommender/TfgLeagueRecommenderApplicationTests.java
@@ -0,0 +1,11 @@
+package es.uja.tfgleaguerecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgLeagueRecommenderApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
